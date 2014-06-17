bfe33a7f0016c49f836bda17eb1fdaaecca18d77
hamzahrmalik <hamzahrmalik@yahoo.co.uk>
2014-06-17 20:00:01 +0100

    Version 1.0
    Initial Release

---
+++ src/com/hamzah/forcefastscroll/Main.java
@@ -0,0 +1,21 @@
+package com.hamzah.forcefastscroll; 
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+ 
+public class Main implements IXposedHookLoadPackage{
+
+	@Override
+	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+		findAndHookMethod("android.widget.AbsListView", lpparam.classLoader, "setFastScrollEnabled", boolean.class, new XC_MethodHook(){
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param)
+					throws Throwable {
+				param.args[0] = true;
+			}
+		});
+	} 
+ 
+}
