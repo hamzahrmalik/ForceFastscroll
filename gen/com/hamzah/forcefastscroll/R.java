20219215da820ebc59469cca099b99e213ee3da8
hamzahrmalik <hamzahrmalik@yahoo.co.uk>
2014-08-18 13:42:07 +0100

    Version 1.1 and 1.2

--- gen/com/hamzah/forcefastscroll/R.java
+++ gen/com/hamzah/forcefastscroll/R.java
@@ -13,8 +13,29 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int app_list=0x7f070000;
+        public static final int button1=0x7f070001;
+        public static final int button2=0x7f070003;
+        public static final int button3=0x7f070004;
+        public static final int button4=0x7f070005;
+        public static final int menu_not_working=0x7f070006;
+        public static final int textView1=0x7f070002;
+    }
+    public static final class layout {
+        public static final int activity_force_fastscroll=0x7f030000;
+    }
+    public static final class menu {
+        public static final int main=0x7f060000;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+        public static final int info=0x7f040002;
+        public static final int invert=0x7f040004;
+        public static final int save=0x7f040005;
+        public static final int select_all=0x7f040003;
+        public static final int title_activity_force_fastscroll=0x7f040001;
+        public static final int xda=0x7f040006;
     }
     public static final class style {
         /** 
@@ -38,10 +59,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
--- src/com/hamzah/forcefastscroll/Main.java
+++ src/com/hamzah/forcefastscroll/Main.java
@@ -2,20 +2,33 @@ package com.hamzah.forcefastscroll;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
  
-public class Main implements IXposedHookLoadPackage{
+public class Main implements IXposedHookLoadPackage, IXposedHookZygoteInit{
 
+	XSharedPreferences pref;
+	
 	@Override
-	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 		findAndHookMethod("android.widget.AbsListView", lpparam.classLoader, "setFastScrollEnabled", boolean.class, new XC_MethodHook(){
 			@Override
 			protected void beforeHookedMethod(MethodHookParam param)
 					throws Throwable {
-				param.args[0] = true;
+				pref.reload();
+				boolean on = pref.getBoolean(lpparam.packageName, false);
+				//XposedBridge.log("FASTSCROLL FOR " + lpparam.packageName  + (on ? "on":"off"));
+				if(on)
+					param.args[0] = true;
 			}
 		});
+	}
+
+	@Override
+	public void initZygote(StartupParam startupParam) throws Throwable {
+		pref = new XSharedPreferences(this.getClass().getPackage().getName(), Keys.PREF_NAME);
 	} 
  
 }
